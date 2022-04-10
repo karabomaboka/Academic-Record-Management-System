116ecc16cfab089c23e027a135e38cebc5abff03
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-04-10 17:44:04 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,38 @@
+<?php $pagename='ABOUT'; ?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include 'components/head.html' ?>
+	<title>About</title>
+
+</head>
+<body>
+    
+	<?php include 'components/header.php' ?>
+	<div class="container-fluid" id="main-para">
+        <div class="container" >
+    	<h2><strong>About</strong></h2>
+    	<p>This is a project based on content management system.</p>
+        </div>
+        <hr>
+        <div class="container">
+          <br>
+            <h4><b>Title:</b></h4>
+            <p>Academic Publications Management System</p><br>
+
+            <h4><b>Introduction:</b></h4>
+            <p>This is a web-based Content Management System which manages publications, patents and projects by the college students and faculty.</p><br>
+
+            <br>
+
+        </div>
+    </div>
+    <?php include 'components/footer.html'?>
+
+
+
+</body>
+</html>
